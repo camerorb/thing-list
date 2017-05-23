c5c7e4025cba3598eb49d395a98b2c89cd8f9fc3
Ryan Cameron <camerorb@grace.edu>
2017-05-23 17:02:57 -0400

    look good

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,14 @@
-import React, from 'react'
+import React from 'react'
 
-const Header =()=> {
-    return (
+import './Header.css'
 
-    )
+const Header = () => {
+  return (
+    <header>
+      <h1>ThingList</h1>
+      <h2>So Many Things</h2>
+    </header>
+  )
 }
-export defult Header 
+
+export default Header
